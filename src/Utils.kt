c449e90ea88d0c21e56fe28059916fea331a7a59
mofeejegi <mofeejegi@gmail.com>
2021-12-04 19:16:46 +0100

    New Util method to read single first line

--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,11 @@ import java.security.MessageDigest
 fun readInput(name: String, day: Int) = File("src/day$day", "$name.txt").readLines()
 
 /**
+ * Read just a single line from the given input txt file
+ */
+fun readInputLine(name: String, day: Int): String = File("src/day$day", "$name.txt").bufferedReader().use { it.readLine() }
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
